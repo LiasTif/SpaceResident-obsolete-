62fc470b74c0a437cc8bafbc463b5bdadcbee839
Lias Tif <ltif427@gmail.com>
2023-10-22 15:58:50 +0300

    Adde mouse hover sound effect to main menu buttons

--- SpaceResidentClient/Views/MainMenu/MainMenuView.xaml.cs
+++ SpaceResidentClient/Views/MainMenu/MainMenuView.xaml.cs
@@ -1,4 +1,6 @@
 ﻿using System.Windows.Controls;
+using System.Windows.Media;
+using System;
 
 namespace SpaceResidentClient.Views.MainMenu
 {
@@ -7,9 +9,18 @@ namespace SpaceResidentClient.Views.MainMenu
     /// </summary>
     public partial class MainMenuView : UserControl
     {
+        private MediaPlayer _player = new();
+
         public MainMenuView()
         {
             InitializeComponent();
+        }
+
+        private void Button_MouseEnter(object sender, System.Windows.Input.MouseEventArgs e)
+        {
+            _player.Open(new Uri("Data/sounds/UI/Button Hover.wav", UriKind.Relative));
+            _player.Volume = 0.2;
+            _player.Play();
         }
     }
 }
