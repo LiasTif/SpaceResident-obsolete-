c63ca4c58ca6f93a1f17b14a80496c8dd84fd8bd
Lias Tif <ltif427@gmail.com>
2023-11-10 22:19:50 +0200

    Added console hiding whenever settings menu is open

---
+++ Converters/MainMenu/HideConsoleConverter.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Globalization;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+
+namespace Converters.MainMenu
+{
+    public class HideConsoleConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value == null)
+                return Visibility.Visible;
+            else
+                return Visibility.Hidden;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotSupportedException();
+        }
+    }
+}
