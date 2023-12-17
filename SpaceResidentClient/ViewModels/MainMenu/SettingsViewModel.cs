5dab38e477252f516ba410bf8b717993863d157b
Lias Tif <ltif427@gmail.com>
2023-12-17 01:42:42 +0100

    Navigation buttons in settings menu have realization now

--- SpaceResidentClient/ViewModels/MainMenu/SettingsViewModel.cs
+++ SpaceResidentClient/ViewModels/MainMenu/SettingsViewModel.cs
@@ -5,6 +5,7 @@ using SpaceResidentClient.ViewModels.Windows;
 using System.Collections.ObjectModel;
 using System.Windows.Controls;
 using System.Windows.Input;
+using Windows.Foundation.Metadata;
 
 namespace SpaceResidentClient.ViewModels.MainMenu
 {
@@ -24,9 +25,43 @@ namespace SpaceResidentClient.ViewModels.MainMenu
         private void Close() => _mainMenuViewModelInstance.SettingsViewSwitch();
         private void OpenGamePage() => CurrentPage = new GameSettingsViewModel();
         private void OpenVideoPage() => CurrentPage = new VideoSettingsViewModel(_mainWindowViewModel);
+        private void PreviousPage()
+        {
+            NavigatePage(false);
+        }
+
+        private void NextPage()
+        {
+            NavigatePage(true);
+        }
+
+        private void NavigatePage(bool isNext)
+        {
+            if (MenuButtons.Count < 2)
+                return;
+
+            for (int i = 0; i < MenuButtons.Count; i++)
+            {
+                if (isNext && i != MenuButtons.Count - 1 && MenuButtons[i].IsChecked == true)
+                {
+                    MenuButtons[i].IsChecked = false;
+                    MenuButtons[i + 1].IsChecked = true;
+                    MenuButtons[i + 1].Command.Execute(null);
+                }
+                else if (!isNext && i - 1 >= 0 && MenuButtons[i].IsChecked == true)
+                {
+                    MenuButtons[i].IsChecked = false;
+                    MenuButtons[i - 1].IsChecked = true;
+                    MenuButtons[i - 1].Command.Execute(null);
+                }
+            }
+        }
+
         public ICommand CloseCommand { get; }
         public ICommand OpenGamePageCommand { get; }
         public ICommand OpenVideoPageCommand { get; }
+        public ICommand PreviousPageCommand { get; }
+        public ICommand NextPageCommand { get; }
         #endregion
 
         public SettingsViewModel(MainMenuViewModel mainMenuViewModel, MainWindowViewModel mainWindowViewModel)
@@ -37,8 +72,10 @@ namespace SpaceResidentClient.ViewModels.MainMenu
             CloseCommand = new RelayCommand(Close);
             OpenGamePageCommand = new RelayCommand(OpenGamePage);
             OpenVideoPageCommand = new RelayCommand(OpenVideoPage);
+            PreviousPageCommand = new RelayCommand(PreviousPage);
+            NextPageCommand = new RelayCommand(NextPage);
 
-            menuButtons =
+            MenuButtons =
             [
                 new() {
                     Content = Properties.Lang.game,
